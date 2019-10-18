dd715de4d8a3cbd67c630a53e32ad7bdcda9eafe
rabdumalikov <rabdumalikov@luxoft.com>
2019-10-18 11:49:08 -0400

    added copyrights

--- include/eswitch/eswitch_v4.hpp
+++ include/eswitch/eswitch_v4.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
+//  Copyright 2019 Rustam Abdumalikov.
+//
+//  "eswitch_v4" library
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #include <tuple>
 #include <array>
 #include <cassert>
--- tests/eswitch_tests.cpp
+++ tests/eswitch_tests.cpp
@@ -1,3 +1,12 @@
+//  Copyright 2019 Rustam Abdumalikov.
+//
+//  "eswitch_v4" library
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #include "eswitch_v4.hpp"
 #include "gtest/gtest.h"
 #include <memory>
