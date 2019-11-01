336d471481f850c3b9dcbb151fd1cb19ce2acaa4
rabdumalikov <rabdumalikov@luxoft.com>
2019-11-01 12:30:52 -0400

    updated LICENSE

--- benchmarks/eswitch_benchmark.cpp
+++ benchmarks/eswitch_benchmark.cpp
@@ -1,11 +1,10 @@
-//  Copyright 2019 Rustam Abdumalikov.
+//  Copyright (c) 2019 Rustam Abdumalikov
 //
 //  "eswitch_v4" library
 //
-//  Use, modification and distribution is subject to the
-//  Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #include "benchmark/benchmark.h"
 #include "eswitch_v4.hpp"
--- example/example8.cpp
+++ example/example8.cpp
@@ -1,3 +1,11 @@
+//  Copyright (c) 2019 Rustam Abdumalikov
+//
+//  "eswitch_v4" library
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #undef NDEBUG
 #include <assert.h>
 
@@ -20,7 +28,7 @@ int main( /* EXAMPLE NAME: ANY ORDER OF MATCHING ARGUMENTS IN CASE */ )
         eswitch( p1, p2, p3 ) >>
             case_( _1 == california && _3 == new_york && _2 == washington ) >> [&]{ executed = true; } >>
             default_ >> []{ assert( false ); };
-            
+
         assert( executed );
     }
 
