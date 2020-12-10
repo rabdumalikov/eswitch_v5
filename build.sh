312cca38c490c86f7df3931bf1c88abf6911d7b7
arustam <vigosslive2@gmail.com>
2020-12-10 11:31:14 -0800

    change the way how compiler is specified

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 mkdir release
 cd release
-cmake ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CROSSCOMPILING=1 -DRUN_HAVE_STD_REGEX=0 -DRUN_HAVE_POSIX_REGEX=0
+cmake ../.. -DCMAKE_BUILD_TYPE=Release
 make
